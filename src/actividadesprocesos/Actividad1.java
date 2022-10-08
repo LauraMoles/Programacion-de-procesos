2c31cfffa0add85c031d5074a066383226133606
Laura Martínez Moles <lauramartinezmoles@gmail.com>
2022-10-09 00:40:27 +0200

    simplificado

--- src/actividadesprocesos/Actividad1.java
+++ src/actividadesprocesos/Actividad1.java
@@ -8,7 +8,6 @@ package actividadesprocesos;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -16,39 +15,22 @@ import java.util.logging.Logger;
  *
  * @author laura
  */
-public class Actividad1 extends Thread {
+public class Actividad1{
 
     public static void main(String args[]) {
         ProcessBuilder pb;
-        String out = null;
-        Process proc = null;
-        StringBuilder sb = new StringBuilder();
-        String line;
+        Process proc;
+        BufferedReader br;
 
         pb = new ProcessBuilder("cmd.exe", "/C", "python jsonJDT.py");
 
         try {
-
             proc = pb.start();
-            proc.waitFor();
-        } catch (InterruptedException ex) {
-            Logger.getLogger(Actividad1.class.getName()).log(Level.SEVERE, null, ex);
+            br = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+            System.out.println(br.readLine());            
+            br.close();
         } catch (IOException ex) {
             Logger.getLogger(Actividad1.class.getName()).log(Level.SEVERE, null, ex);
         }
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(proc.getInputStream()));
-        try {
-            while ((line = br.readLine()) != null) {
-                sb.append(line).append("\n");
-            }
-            br.close();
-            out = sb.toString();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println(out);
-
     }
 }
